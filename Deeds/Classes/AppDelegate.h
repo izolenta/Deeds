f3d54d1e98b24d20ce098eda61228b5a4c90481e
alea <lehtenstaarn@gmail.com>
2014-03-19 00:04:42 +0400

    Initial Commit

---
+++ Deeds/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Deeds
+//
+//  Created by Alexey on 19.03.14.
+//  Copyright Swiftmoon Software 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Deeds/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Deeds
+//
+//  Created by Alexey on 19.03.14.
+//  Copyright Swiftmoon Software 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
